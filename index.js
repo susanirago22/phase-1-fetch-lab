aba4b7cac14d157f11d98b30f7e4475be7da25f9
susanirago22 <susaniragoorani@gmail.com>
2022-12-08 11:02:07 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const returnFetch = fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
+  return returnFetch;
 }
 
 function renderBooks(books) {
@@ -15,3 +18,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
